fd8ec01811a280e241266f25c47c6e8a4fe4cd00
sandeepsaripalli <hawks900@gmail.com>
2023-09-06 12:10:15 -0400

    added signout features

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,19 @@
 import React, { useRef, useState } from 'react'
 import HeaderComponent from './HeaderComponent'
 import {checkValidData} from '../utils/validate'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import {auth} from '../utils/firebase'
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 const Login = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
     const [isSignInForm, setIsSignInForm] = useState(true);
     const [errorMessage, setErrorMessage] = useState(null)
     const email = useRef(null)
     const password = useRef(null)
+    const name = useRef(null)
     const toggleSignInForm=  () =>{
         setIsSignInForm(!isSignInForm);
 
@@ -23,7 +29,20 @@ const Login = () => {
             .then((userCredential) => {
               // Signed in
               const user = userCredential.user;
-              console.log(user);
+              updateProfile(user, {
+                displayName: name.current.value, photoURL: "https://robohash.org/" + `${name.current.value}`
+              }).then(() => {
+                // Profile updated!
+
+                // if thhis below code is not done we will see empty phto url and display name during first login redirect
+                const {uid, email, displayName, photoURL} = auth.currentUser;
+          dispatch(addUser({uid: uid, email: email, displayName: displayName, photoURL: photoURL}));
+                navigate("/browse")
+              }).catch((error) => {
+                // An error occurred
+                setErrorMessage(error.message)
+              });
+             
             })
             .catch((error) => {
               const errorCode = error.code;
@@ -36,6 +55,7 @@ const Login = () => {
               // Signed in
               const user = userCredential.user;
               console.log(user)
+              navigate("/browse")
             })
             .catch((error) => {
               const errorCode = error.code;
@@ -55,7 +75,7 @@ const Login = () => {
     </div>
     <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-75" onSubmit={(e) =>e.preventDefault()}>
     <h1 className='font-bold text-3xl py-4'>{isSignInForm ? 'Sign In' : "Sign up"}</h1>
-    {!isSignInForm && (<input type="text" placeholder='Full name' className='p-4 my-4 w-full bg-gray-800' />)}
+    {!isSignInForm && (<input ref = {name} type="text" placeholder='Full name' className='p-4 my-4 w-full bg-gray-800' />)}
         <input ref = {email} type="email" placeholder='email' className='p-4 my-4 w-full bg-gray-800' />
         <input ref = {password} type="password" placeholder='password' className='p-4 my-4 w-full bg-gray-800' />
         <p className='text-red-500 font-bold text-lg py-2'>{errorMessage}</p>
