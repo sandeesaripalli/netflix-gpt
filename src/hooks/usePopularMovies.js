c942e808822140a86650caae666518627f319150
sandeepsaripalli <hawks900@gmail.com>
2023-09-07 14:18:07 -0400

    fixed build error

--- src/hooks/usePopularMovies.js
+++ src/hooks/usePopularMovies.js
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { useDispatch } from "react-redux";
-import {  addPopularMovies } from "../utils/moviesSlice";
+import { addPopularMovies } from "../utils/moviesSlice";
 import { API_OPTIONS } from "../utils/constants";
 
-const usePopularMovies = () =>{
-    const dispatch = useDispatch()
-  useEffect (() =>{
-    getPopularMovies();
+const usePopularMovies = () => {
+  const dispatch = useDispatch();
 
-
-  },[])
-
-  const getPopularMovies = async () =>{
-    const data = await fetch('https://api.themoviedb.org/3/movie/popular?page=1', API_OPTIONS)
+  const getPopularMovies = useCallback(async () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/popular?page=1', API_OPTIONS);
     const json = await data.json();
-   
-    dispatch(addPopularMovies(json.results))
-  }
-}
+    dispatch(addPopularMovies(json.results));
+  }, [dispatch]); // Include only the dependencies needed by the function
 
-export default usePopularMovies
+  useEffect(() => {
+    getPopularMovies();
+  }, [getPopularMovies]);
+
+  return getPopularMovies;
+};
+
+export default usePopularMovies;
